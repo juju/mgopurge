3933168e5d660bb7819d3dbe18fb213cd0ff7937
Menno Smits <menno@freshfoo.com>
2016-01-06 11:33:23 +1300

    Make mgopurge more user friendly
    - Add some basic command line checking.
    - Emit some progress information.
    - If no collections are specified, check them all.

--- mgopurge.go
+++ mgopurge.go
@@ -5,10 +5,13 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/txn"
 )
+
+const txnsC = "txns"
 
 func checkErr(label string, err error) {
 	if err != nil {
@@ -24,8 +27,51 @@ func (l *fakeLogger) Output(_ int, s string) error {
 	return nil
 }
 
+func printUsageAndExit() {
+	fmt.Println(`
+usage: mgopurge <password> [collections...]
+
+If no collections are specified, all of Juju's collections will be 
+checked for orphaned transactions.
+`[1:])
+	os.Exit(1)
+}
+
+func processArgs(args []string) (password string, collections []string) {
+	if len(args) < 1 {
+		printUsageAndExit()
+	}
+	password = args[0]
+	collections = args[1:]
+	return
+}
+
+func isPurgeableCollection(name string) bool {
+	if name == txnsC {
+		return false
+	}
+	if name == "system.indexes" {
+		return false
+	}
+	if strings.HasPrefix(name, txnsC+".") {
+		return false
+	}
+	return true
+}
+
+func getAllPurgeableCollections(db *mgo.Database) (collections []string) {
+	allCollections, err := db.CollectionNames()
+	checkErr("CollectionNames", err)
+	for _, collection := range allCollections {
+		if isPurgeableCollection(collection) {
+			collections = append(collections, collection)
+		}
+	}
+	return
+}
+
 func main() {
-	collections := os.Args[2:]
+	password, collections := processArgs(os.Args[1:])
 
 	tlsConfig := &tls.Config{
 		InsecureSkipVerify: true,
@@ -42,7 +88,7 @@ func main() {
 		Addrs:      []string{"127.0.0.1:37017"},
 		Database:   "admin",
 		Username:   "admin",
-		Password:   os.Args[1],
+		Password:   password,
 		DialServer: dial,
 	}
 	session, err := mgo.DialWithInfo(info)
@@ -50,8 +96,17 @@ func main() {
 
 	txn.SetLogger(&fakeLogger{})
 
-	txns := session.DB("juju").C("txns")
-	runner := txn.NewRunner(txns)
+	// If the user didn't specify collections on the command line,
+	// inspect the DB to find them all.
+	db := session.DB("juju")
+	if len(collections) == 0 {
+		collections = getAllPurgeableCollections(db)
+	}
+
+	runner := txn.NewRunner(db.C(txnsC))
+	fmt.Printf("Purging orphaned transactions for: %s\n",
+		strings.Join(collections, ", "))
 	err = runner.PurgeMissing(collections...)
 	checkErr("PurgeMissing", err)
+	fmt.Println("Done!")
 }
