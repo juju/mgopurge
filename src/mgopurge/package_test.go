e8630e415a157f66ff490f87dcf9c351325530a5
John Arbash Meinel <john@arbash-meinel.com>
2017-07-05 11:28:53 +0400

    Start building tests for mgopurge.
    Needs dependency updates.

---
+++ src/mgopurge/package_test.go
@@ -0,0 +1,13 @@
+// Copyright 2017 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+package main_test
+
+import (
+	"testing"
+
+	jujutesting "github.com/juju/testing"
+)
+
+func TestPackage(t *testing.T) {
+	jujutesting.MgoTestPackage(t, nil)
+}
